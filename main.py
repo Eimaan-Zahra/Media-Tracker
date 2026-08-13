ca122b7df432135b8c0091d86f114916708b19d8
Eimaan Zahra <emaanzahra454@gmail.com>
2026-08-13 13:35:56 +0500

    Add book movie and game models

--- main.py
+++ main.py
@@ -2,5 +2,37 @@
 
 from models import Media
 
-test_item = Media("How to train your dragon", "watched",5)
-print(test_item)
+
+
+
+from models import Book, Movie, Game
+
+book = Book(
+    "The Hobbit",
+    status="finished",
+    rating=5,
+    pages=310,
+    author="J. R. R. Tolkien"
+)
+
+movie = Movie(
+    "Inception",
+    status="watched",
+    rating=4,
+    runtime_minutes=148,
+    director="Christopher Nolan"
+)
+
+game = Game(
+    "Minecraft",
+    status="in-progress",
+    rating=5,
+    hours_played=42,
+    platform="PC"
+)
+
+print(book)
+print()
+print(movie)
+print()
+print(game)
--- models.py
+++ models.py
@@ -3,17 +3,52 @@
 from datetime import date
 
 class Media:
-    def __init__(self,title,status='Not started',rating=None):
+    def __init__(self,title,status='not-started',rating=None):
         self.title=title
         self.status=status
         self.rating=rating
-        self.date=date.today()
+        self.date_added=date.today()
 
     def __str__(self):
-        return f'Title : {self.title} \nStatus : {self.status} \nRating : {self.rating} \nDate added : {self.date}'
+        return f'Title : {self.title} \nStatus : {self.status} \nRating : {self.rating} \nDate added : {self.date_added}'
 
-    def __rep__(self):
-            return f'Title : {self.title!r} \nStatus : {self.status!r} \nRating : {self.rating!r} \nDate added : {self.date}'
-
-
+    def __repr__(self):
+        return f'Title : {self.title!r} \nStatus : {self.status!r} \nRating : {self.rating!r} \nDate added : {self.date_added}'
     
+class Book(Media):
+
+    def __init__(self,title,*,status='not-started',rating=None,pages,author):
+        super().__init__(title,status,rating)
+        self.pages=pages
+        self.author=author
+
+    def __str__(self):   
+        return super().__str__() + f'\nPages : {self.pages} \nAuthor : {self.author}'
+    
+    def __repr__(self):
+        return super().__repr__() + f'\nPages : {self.pages!r} \nAuthor : {self.author!r}'
+
+class Movie(Media):
+
+    def __init__(self,title,*,status='not-started',rating=None,runtime_minutes,director):
+          super().__init__(title,status,rating)
+          self.runtime_minutes=runtime_minutes
+          self.director=director
+
+    def __str__(self):   
+        return super().__str__() + f'\nRun Time Minutes : {self.runtime_minutes} \nDirector : {self.director}'
+    
+    def __repr__(self):
+        return super().__repr__() + f'\nRun Time Minutes : {self.runtime_minutes!r} \nDirector : {self.director!r}'
+class Game(Media):
+
+    def __init__(self,title,*,status='not-started',rating=None,hours_played,platform):
+          super().__init__(title,status,rating)
+          self.hours_played=hours_played
+          self.platform=platform         
+
+    def __str__(self):   
+        return super().__str__() + f'\nHours Played : {self.hours_played} \nPlatform : {self.platform}'
+    
+    def __repr__(self):
+        return super().__repr__() + f'\nHours Played : {self.hours_played!r} \nPlatform : {self.platform!r}'
