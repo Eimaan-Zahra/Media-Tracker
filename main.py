e1cf380ead0ab99cadfb10e86cf9a151321d6d64
Eimaan Zahra <emaanzahra454@gmail.com>
2026-08-13 12:35:16 +0500

    Add Media base class

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 """Command-line entry point for the media tracker."""
 
+from models import Media
+
+test_item = Media("How to train your dragon", "watched",5)
+print(test_item)
--- models.py
+++ models.py
@@ -1,2 +1,19 @@
 """Media model classes for the media tracker."""
 
+from datetime import date
+
+class Media:
+    def __init__(self,title,status='Not started',rating=None):
+        self.title=title
+        self.status=status
+        self.rating=rating
+        self.date=date.today()
+
+    def __str__(self):
+        return f'Title : {self.title} \nStatus : {self.status} \nRating : {self.rating} \nDate added : {self.date}'
+
+    def __rep__(self):
+            return f'Title : {self.title!r} \nStatus : {self.status!r} \nRating : {self.rating!r} \nDate added : {self.date}'
+
+
+    
